8319764a2b0fd21e2cfb3b7e8c7b10e6abab0c0e
cagri.yolyapar <cagri.yolyapar@gmail.com>
2025-05-06 14:04:32 +0300

    Add project files.

---
+++ CoreMVCIntro_2/Controllers/DenemeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCIntro_2.Controllers
+{
+
+    //MVC olay güdümlü degil metot güdümlü calısan bir mimari patterndir...
+
+    //MVC'deki Controller icindeki metotlara Action deriz...
+    public class DenemeController : Controller
+    {
+
+        //Actionlar ya size bir sayfa döndürür ya da sizi baska bir sayfaya yönlendirir...Eger return View(); ile bir Action sonlanıyorsa bu size bir sayfa döndürüyor demektir...return RedirectToAction() ile sonlanıyorsa sizi baska bir Action'a(Dolayısıyla baska bir sayfaya) yonlendiriyor demektir...Ama sakın karıstırmayın View ile Action aynı şeyler asla degildir...
+
+        //Eger bir Action bir sayfayı return View() kodu ile döndürüyorsa Views klasorunde kendisiyle aynı isme denk gelen bir View döndürmeye calısır... Bu View sayfasının isminin Index olması lazım (cünkü Action'imizin ismi Index) ve aynı zamanda bu View sayfasının Views klasorunde ilgili Controller ismindeki bir klasorde bulunması lazım...
+        public IActionResult Index()
+        {
+            //View() metodu eger ona bir string argüman vermezseniz icinde bulundugu Action isminde bir View döndürmeye calısır...
+            return View();
+        }
+    }
+}
---
+++ CoreMVCIntro_2/Program.cs
@@ -0,0 +1,51 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//MiddleWare: Sistemin calısma mekanizmasını icerisinde bulunmasını istediginiz servisler eklenir...
+
+
+
+
+
+
+
+var app = builder.Build();
+
+//PipeLine
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Deneme}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
+
+
+
+
+
+
+
+
+
+//www.cagri.com
+
+//localhost:7552/ControllerIsmi/ActionIsmi
